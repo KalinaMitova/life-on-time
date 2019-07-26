c6168a18b96ef0d0ed8fddf0903594d28366541a
KalinaMitova <kalina.mitova@gmail.com>
2019-07-26 09:04:48 +0300

    create progress-dashboard module and routing module

---
+++ src/app/pages/progress-dashboard/progress-dashboard.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProgressDashboardRoutingModule } from './progress-dashboard-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProgressDashboardRoutingModule
+  ]
+})
+export class ProgressDashboardModule { }

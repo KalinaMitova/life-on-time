2d7ee650b8e2054264e133ccf090edde240dae7e
KalinaMitova <kalina.mitova@gmail.com>
2019-07-29 20:27:12 +0300

    create full-pages folder

---
+++ src/app/pages/full-pages/full-pages.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FullPagesRoutingModule } from './full-pages-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FullPagesRoutingModule
+  ]
+})
+export class FullPagesModule { }

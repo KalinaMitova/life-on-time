9d54fa67f6387ebd1e4b055c02b52dd76bcebe18
KalinaMitova <kalina.mitova@gmail.com>
2019-07-29 20:25:20 +0300

    create content-pages folder

---
+++ src/app/pages/content-pages/content-pages.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContentPagesRoutingModule } from './content-pages-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ContentPagesRoutingModule
+  ]
+})
+export class ContentPagesModule { }

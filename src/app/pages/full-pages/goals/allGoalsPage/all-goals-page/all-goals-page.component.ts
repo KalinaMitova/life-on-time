79683118eed75307166c06fa818f0fb39438bba3
KalinaMitova <kalina.mitova@gmail.com>
2019-11-05 15:33:02 +0200

    fix export to pdf

--- src/app/pages/full-pages/goals/allGoalsPage/all-goals-page/all-goals-page.component.ts
+++ src/app/pages/full-pages/goals/allGoalsPage/all-goals-page/all-goals-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { GoalService } from 'app/shared/services/goal.service';
 import { Observable } from 'rxjs';
 import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component( {
   selector: 'app-all-goals-page',
@@ -20,22 +21,16 @@ export class AllGoalsPageComponent implements OnInit {
     this.goals$ = this.goalService.getAllUserGoals();
   }
 
-  public downloadAsPDF() {
-    console.log( this.allGoals );
-    const doc = new jsPDF();
+  downloadAsPDF() {
+    html2canvas( this.allGoals.nativeElement ).then( canvas => {
+      const pdf = new jsPDF();
+      const img = canvas.toDataURL( "image/png" );
+      const imgProps = pdf.getImageProperties( img );
+      const pdfWidth = pdf.internal.pageSize.getWidth() * 0.83;
+      const pdfHeight = imgProps.height / 4 > pdf.internal.pageSize.getHeight() * 0.9 ? pdf.internal.pageSize.getHeight() * 0.9 : imgProps.height / 4;
 
-    const specialElementHandlers = {
-      '#editor': function ( element, renderer ) {
-        return true;
-      }
-    };
-
-    const pdfGoals = this.allGoals.nativeElement;
-    doc.fromHTML( pdfGoals.innerHTML, 15, 15, {
-      width: 170,
-      'elementHandlers': specialElementHandlers
-    } );
-    doc.save( 'all-goals.pdf' );
+      pdf.addImage( img, 'PNG', 20, 20, pdfWidth, pdfHeight );
+      pdf.save( 'goals.pdf' );
+    } )
   }
-
 }
